bbf20928d8b782e4a104da179e19f0769288df88
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-23 23:55:42 +0300

    style: indent

--- src/App.tsx
+++ src/App.tsx
@@ -4,79 +4,79 @@ import "./App.css";
 import { type Todo } from "./types/todo.type";
 
 function App() {
-	const [todos, setTodos] = React.useState<Todo[]>([]);
-	const [isLoading, setLoading] = React.useState(true);
-	const [todoUpdating, setTodoUpdating] = React.useState(false);
+  const [todos, setTodos] = React.useState<Todo[]>([]);
+  const [isLoading, setLoading] = React.useState(true);
+  const [todoUpdating, setTodoUpdating] = React.useState(false);
 
-	function loadTodos() {
-		setTodoUpdating(true);
+  function loadTodos() {
+    setTodoUpdating(true);
 
-		todosApi
-			.getAll()
-			.then(setTodos)
-			.finally(() => {
-				setLoading(false);
-				setTodoUpdating(false);
-			});
-	}
+    todosApi
+      .getAll()
+      .then(setTodos)
+      .finally(() => {
+        setLoading(false);
+        setTodoUpdating(false);
+      });
+  }
 
-	async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-		e.preventDefault();
-		setTodoUpdating(true);
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setTodoUpdating(true);
 
-		const form = e.currentTarget;
-		const formData = new FormData(form);
-		const title = formData.get("title");
+    const form = e.currentTarget;
+    const formData = new FormData(form);
+    const title = formData.get("title");
 
-		if (title) {
-			const newTodo = await todosApi.add(title);
-			setTodos([...todos, newTodo]);
-			form.reset();
-		}
+    if (title) {
+      const newTodo = await todosApi.add(title);
+      setTodos([...todos, newTodo]);
+      form.reset();
+    }
 
-		setTodoUpdating(false);
-	}
+    setTodoUpdating(false);
+  }
 
-	async function handleRemove(todoId: string) {
-		setTodoUpdating(true);
+  async function handleRemove(todoId: string) {
+    setTodoUpdating(true);
 
-		await todosApi.remove(todoId).finally(() => {
-			setTodoUpdating(false);
-		});
-		loadTodos();
-	}
+    await todosApi.remove(todoId).finally(() => {
+      setTodoUpdating(false);
+    });
+    loadTodos();
+  }
 
-	React.useEffect(() => {
-		loadTodos();
-	}, []);
+  React.useEffect(() => {
+    loadTodos();
+  }, []);
 
-	if (isLoading) {
-		return <h2>Loading...</h2>;
-	}
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
 
-	return (
-		<main>
-			<form onSubmit={async (e) => handleSubmit(e)}>
-				<input type='text' name='title' required placeholder='add todo' />
-			</form>
-			<h1>Todos</h1>
-			{todos.map((todo) => (
-				<p key={todo.id}>
-					<>
-						{todo.title} {todo.completed ? "done " : "not yet "}
-						<button
-							type='button'
-							name='remove'
-							onClick={async () => handleRemove(todo.id)}
-							disabled={todoUpdating}
-						>
-							{todoUpdating ? "Wait..." : "Remove"}
-						</button>
-					</>
-				</p>
-			))}
-		</main>
-	);
+  return (
+    <main>
+      <form onSubmit={async (e) => handleSubmit(e)}>
+        <input type="text" name="title" required placeholder="add todo" />
+      </form>
+      <h1>Todos</h1>
+      {todos.map((todo) => (
+        <p key={todo.id}>
+          <>
+            {todo.title} {todo.completed ? "done " : "not yet "}
+            <button
+              type="button"
+              name="remove"
+              onClick={async () => handleRemove(todo.id)}
+              disabled={todoUpdating}
+            >
+              {todoUpdating ? "Wait..." : "Remove"}
+            </button>
+          </>
+        </p>
+      ))}
+    </main>
+  );
 }
 
 export default App;
--- src/api/todos.ts
+++ src/api/todos.ts
@@ -4,46 +4,46 @@ import { Todo } from "../types/todo.type.ts";
 axios.defaults.baseURL = "http://localhost:3000";
 
 function handleError(error: any) {
-	if (error.response) {
-		// The request was made and the server responded with a status code
-		// that falls out of the range of 2xx
-		console.log(error.response.statusText);
-		console.log(error.response.status);
-	} else if (error.request) {
-		// The request was made but no response was received
-		// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-		// http.ClientRequest in node.js
-		console.log(error.request);
-	} else {
-		// Something happened in setting up the request that triggered an Error
-		console.log("Error", error.message);
-	}
+  if (error.response) {
+    // The request was made and the server responded with a status code
+    // that falls out of the range of 2xx
+    console.log(error.response.statusText);
+    console.log(error.response.status);
+  } else if (error.request) {
+    // The request was made but no response was received
+    // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+    // http.ClientRequest in node.js
+    console.log(error.request);
+  } else {
+    // Something happened in setting up the request that triggered an Error
+    console.log("Error", error.message);
+  }
 }
 
 export function getAll(): Promise<Todo[]> {
-	return axios
-		.get("/api_v1/todos")
-		.then((res) => res.data)
-		.catch(handleError);
+  return axios
+    .get("/api_v1/todos")
+    .then((res) => res.data)
+    .catch(handleError);
 }
 
 export function getOne(todoId: string): Promise<Todo> {
-	return axios
-		.get(`/api_v1/todos/${todoId}`)
-		.then((res) => res.data)
-		.catch(handleError);
+  return axios
+    .get(`/api_v1/todos/${todoId}`)
+    .then((res) => res.data)
+    .catch(handleError);
 }
 
 export function add(title: FormDataEntryValue): Promise<Todo> {
-	return axios
-		.post("/api_v1/todos", { title })
-		.then((res) => res.data)
-		.catch(handleError);
+  return axios
+    .post("/api_v1/todos", { title })
+    .then((res) => res.data)
+    .catch(handleError);
 }
 
 export function remove(todoId: string): Promise<string> {
-	return axios
-		.delete(`/api_v1/todos/${todoId}`)
-		.then((res) => res.data)
-		.catch(handleError);
+  return axios
+    .delete(`/api_v1/todos/${todoId}`)
+    .then((res) => res.data)
+    .catch(handleError);
 }
