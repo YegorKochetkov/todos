2b2ccd9eb26a68acbffbbdcbfe53f35540e8309a
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-24 17:22:31 +0300

    feat: add load state to add form button

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,16 +1,28 @@
 import React from "react";
 
 type AddTodoProps = {
-  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => Promise<void>;
+	onSubmit: (event: React.FormEvent<HTMLFormElement>) => Promise<void>;
 };
 
-function AddTodo({ handleSubmit }: AddTodoProps) {
-  return (
-    <form onSubmit={(event) => handleSubmit(event)} name="add_todo">
-      <input type="text" name="title" required placeholder="add todo" />
-      <input type="submit" value="Add" />
-    </form>
-  );
+function AddTodo({ onSubmit }: AddTodoProps) {
+	const [isAdding, setIsAdding] = React.useState(false);
+
+	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		setIsAdding(true);
+		await onSubmit(event);
+		setIsAdding(false);
+	}
+
+	return (
+		<form onSubmit={(event) => handleSubmit(event)} name='add_todo'>
+			<input type='text' name='title' required placeholder='add todo' />
+			<input
+				type='submit'
+				value={isAdding ? "Wait" : "Add"}
+				disabled={isAdding}
+			/>
+		</form>
+	);
 }
 
 export default AddTodo;
