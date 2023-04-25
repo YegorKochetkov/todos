3d4e932b1e5cf0671021cf647f0f43c1c6efe3b0
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-26 00:18:48 +0300

    feat: move remove todo button to component

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,98 +1,92 @@
 import React from "react";
 import * as todosApi from "../api/todos.ts";
-import TodoCompleteInput from "./TodoCompleteInput.tsx";
-import TodoTitleInput from "./TodoTitleInput.tsx";
+import TodoComplete from "./TodoComplete.tsx";
+import TodoRemove from "./TodoRemove.tsx";
+import TodoTitle from "./TodoTitle.tsx";
 import { type Todo } from "../types/todo.type";
 
 type TodoItemProps = {
-  todo: Todo;
-  onLoad: () => Promise<void>;
+	todo: Todo;
+	onLoad: () => Promise<void>;
 };
 
 function TodoItem({ todo, onLoad }: TodoItemProps) {
-  const [isUpdating, setIsUpdating] = React.useState(false);
-  const [isEditing, setIsEditing] = React.useState(false);
+	const [isUpdating, setIsUpdating] = React.useState(false);
+	const [isEditing, setIsEditing] = React.useState(false);
 
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
+	async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+		event.preventDefault();
 
-    const form = event.currentTarget;
-    const data = new FormData(form);
+		const form = event.currentTarget;
+		const data = new FormData(form);
 
-    const title = data.get("todo")?.toString().trimStart();
+		const title = data.get("todo")?.toString().trimStart();
 
-    if (title?.length === 0) {
-      return;
-    }
+		if (title?.length === 0) {
+			return;
+		}
 
-    if (title && title !== todo.title) {
-      handleUpdate(title);
-    }
+		if (title && title !== todo.title) {
+			handleUpdate(title);
+		}
 
-    setIsEditing(false);
-  }
+		setIsEditing(false);
+	}
 
-  async function handleUpdate(value: string | boolean) {
-    setIsUpdating(true);
-    setIsEditing(false);
+	async function handleUpdate(value: string | boolean) {
+		setIsUpdating(true);
+		setIsEditing(false);
 
-    if (typeof value === "string" && value.length === 0) {
-      setIsUpdating(false);
-      return;
-    }
+		if (typeof value === "string" && value.length === 0) {
+			setIsUpdating(false);
+			return;
+		}
 
-    if (todo.title === value) {
-      setIsUpdating(false);
-      return;
-    }
+		if (todo.title === value) {
+			setIsUpdating(false);
+			return;
+		}
 
-    const updatedTodo = {
-      ...todo,
-    };
+		const updatedTodo = {
+			...todo,
+		};
 
-    if (typeof value === "string") {
-      updatedTodo.title = value;
-    }
+		if (typeof value === "string") {
+			updatedTodo.title = value;
+		}
 
-    if (typeof value === "boolean") {
-      updatedTodo.completed = value;
-    }
+		if (typeof value === "boolean") {
+			updatedTodo.completed = value;
+		}
 
-    await todosApi.update(updatedTodo);
-    await onLoad();
-    setIsUpdating(false);
-  }
+		await todosApi.update(updatedTodo);
+		await onLoad();
+		setIsUpdating(false);
+	}
 
-  async function handleRemove(todoId: string) {
-    setIsUpdating(true);
+	async function handleRemove(todoId: string) {
+		setIsUpdating(true);
 
-    await todosApi.remove(todoId);
-    await onLoad();
-    setIsUpdating(false);
-  }
+		await todosApi.remove(todoId);
+		await onLoad();
+		setIsUpdating(false);
+	}
 
-  return (
-    <li>
-      <form
-        name="todo"
-        onSubmit={(event) => handleSubmit(event)}
-        style={{ display: "flex", gap: "0.25rem", alignItems: "center" }}
-      >
-        <TodoTitleInput
-          {...{ isEditing, setIsEditing, isUpdating, handleUpdate, todo }}
-        />
-        <TodoCompleteInput {...{ handleUpdate, todo }} />
-        <button
-          type="button"
-          name="remove"
-          onClick={() => handleRemove(todo.id)}
-          disabled={isUpdating}
-        >
-          {isUpdating ? "Wait..." : "Remove"}
-        </button>
-      </form>
-    </li>
-  );
+	return (
+		<li>
+			<form
+				name='todo'
+				onSubmit={(event) => handleSubmit(event)}
+				style={{ display: "flex", gap: "0.25rem", alignItems: "center" }}
+			>
+				<TodoTitle
+					{...{ isEditing, setIsEditing, isUpdating, handleUpdate, todo }}
+				/>
+				<TodoComplete {...{ handleUpdate, todo }} />
+				<TodoRemove id={todo.id} {...{ handleRemove, isUpdating }} />
+			</form>
+		</li>
+	);
 }
 
 export default TodoItem;
