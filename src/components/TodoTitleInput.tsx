08d5ec3d1dadb0fdf820c647f2dde2c7290c0e6e
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-25 22:54:54 +0300

    feat: todo edit by space key

--- src/components/TodoTitleInput.tsx
+++ src/components/TodoTitleInput.tsx
@@ -2,65 +2,67 @@ import React from "react";
 import { Todo } from "../types/todo.type";
 
 type TodoTitleInputProps = {
-  isEditing: boolean;
-  setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
-  isUpdating: boolean;
-  handleUpdate: (value: string | boolean) => Promise<void>;
-  todo: Todo;
+	isEditing: boolean;
+	setIsEditing: React.Dispatch<React.SetStateAction<boolean>>;
+	isUpdating: boolean;
+	handleUpdate: (value: string | boolean) => Promise<void>;
+	todo: Todo;
 };
 
 function TodoTitleInput(props: TodoTitleInputProps) {
-  const { isEditing, setIsEditing, todo, isUpdating, handleUpdate } = props;
+	const { isEditing, setIsEditing, todo, isUpdating, handleUpdate } = props;
 
-  return (
-    <React.Fragment>
-      {isEditing ? (
-        <input
-          type="text"
-          name={"todo"}
-          id="todo_title"
-          onBlur={(event) => handleUpdate(event.target.value.trimStart())}
-          onKeyDown={(event) => setIsEditing(!(event.key === "Escape"))}
-          defaultValue={todo.title}
-          style={{
-            boxSizing: "border-box",
-            width: "20rem",
-            height: "2rem",
-            padding: "0.25rem",
-            margin: "0",
-            marginBottom: "0.25rem",
-            borderRadius: "3px",
-            fontSize: "1rem",
-          }}
-          required
-          autoFocus
-        />
-      ) : (
-        <label
-          htmlFor="todo_title"
-          style={{
-            boxSizing: "border-box",
-            display: "inline-block",
-            width: "20rem",
-            height: "2rem",
-            padding: "0.25rem",
-            margin: "0",
-            marginBottom: "0.25rem",
-            border: "2px solid transparent",
-            borderRadius: "3px",
-            fontSize: "1rem",
-          }}
-          onDoubleClick={() => {
-            setIsEditing(true);
-          }}
-          onKeyUpCapture={(event) => setIsEditing(() => event.key === "Enter")}
-          tabIndex={0}
-        >
-          {isUpdating ? "Updating..." : todo.title}
-        </label>
-      )}
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			{isEditing ? (
+				<input
+					type='text'
+					name={"todo"}
+					id='todo_title'
+					onBlur={(event) => handleUpdate(event.target.value.trimStart())}
+					onKeyDown={(event) => setIsEditing(!(event.key === "Escape"))}
+					defaultValue={todo.title}
+					style={{
+						boxSizing: "border-box",
+						width: "20rem",
+						height: "2rem",
+						padding: "0.25rem",
+						margin: "0",
+						marginBottom: "0.25rem",
+						borderRadius: "3px",
+						fontSize: "1rem",
+					}}
+					required
+					autoFocus
+				/>
+			) : (
+				<label
+					htmlFor='todo_title'
+					style={{
+						boxSizing: "border-box",
+						display: "inline-block",
+						width: "20rem",
+						height: "2rem",
+						padding: "0.25rem",
+						margin: "0",
+						marginBottom: "0.25rem",
+						border: "2px solid transparent",
+						borderRadius: "3px",
+						fontSize: "1rem",
+					}}
+					onDoubleClick={() => {
+						setIsEditing(true);
+					}}
+					onKeyUpCapture={(event) =>
+						setIsEditing(event.key === "Enter" || event.key === " ")
+					}
+					tabIndex={0}
+				>
+					{isUpdating ? "Updating..." : todo.title}
+				</label>
+			)}
+		</React.Fragment>
+	);
 }
 
 export default TodoTitleInput;
