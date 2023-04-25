1416b8828a5a9c3c499da92e2cd34ba75af799c6
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-25 13:15:56 +0300

    feat: make todo list as ordered list

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,42 @@ import TodoItem from "./components/TodoItem.tsx";
 import { type Todo } from "./types/todo.type";
 
 function App() {
-  const [todos, setTodos] = React.useState<Todo[]>([]);
-  const [isLoading, setLoading] = React.useState(true);
+	const [todos, setTodos] = React.useState<Todo[]>([]);
+	const [isLoading, setLoading] = React.useState(true);
 
-  function loadTodos() {
-    return todosApi
-      .getAll()
-      .then(setTodos)
-      .finally(() => {
-        setLoading(false);
-      });
-  }
+	function loadTodos() {
+		return todosApi
+			.getAll()
+			.then(setTodos)
+			.finally(() => {
+				setLoading(false);
+			});
+	}
 
-  async function handleAddTodo(title: string) {
-    const newTodo = await todosApi.add(title);
-    setTodos([...todos, newTodo]);
-  }
+	async function handleAddTodo(title: string) {
+		const newTodo = await todosApi.add(title);
+		setTodos([...todos, newTodo]);
+	}
 
-  React.useEffect(() => {
-    loadTodos();
-  }, []);
+	React.useEffect(() => {
+		loadTodos();
+	}, []);
 
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
+	if (isLoading) {
+		return <h2>Loading...</h2>;
+	}
 
-  return (
-    <main>
-      <h1>Todos</h1>
-      <AddTodo onAddTodo={handleAddTodo} />
-      {todos.map((todo) => (
-        <TodoItem key={todo.id} todo={todo} onLoad={loadTodos} />
-      ))}
-    </main>
-  );
+	return (
+		<main>
+			<h1>Todos</h1>
+			<AddTodo onAddTodo={handleAddTodo} />
+			<ol style={{ marginTop: "0.5rem", paddingLeft: "1.5rem" }}>
+				{todos.map((todo) => (
+					<TodoItem key={todo.id} todo={todo} onLoad={loadTodos} />
+				))}
+			</ol>
+		</main>
+	);
 }
 
 export default App;
