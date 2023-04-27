e135e9e49efe6c402af9684d720ec166ce150756
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-27 14:01:10 +0300

    feat: optimize by splitting app.js from index.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
+const App = React.lazy(() => import("./App.tsx"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
