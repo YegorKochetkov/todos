05cc08b586737b45e75673c73dc7969b56369952
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-04-27 17:35:07 +0300

    feat: change complete logic
    now when all filter is checked function reverse todos status
    instead set all todos to complete or active

--- src/components/TodosStatusToggle.tsx
+++ src/components/TodosStatusToggle.tsx
@@ -1,28 +1,47 @@
 import React from "react";
 import * as todosApi from "../api/todos.ts";
 import { Todo } from "../types/todo.type.ts";
+import { Filters } from "./TodosFilter.tsx";
 
 type TodosStatusToggleProps = {
 	todos: Todo[];
 	loadTodos: () => Promise<void>;
+	filter: Filters;
 };
 
-function TodosStatusToggle({ todos, loadTodos }: TodosStatusToggleProps) {
-	const [completeStatus, setCompleteStatus] = React.useState(true);
+function TodosStatusToggle({
+	todos,
+	loadTodos,
+	filter,
+}: TodosStatusToggleProps) {
 	const [isUpdating, setIsUpdating] = React.useState(false);
 
 	function toggleAll() {
 		setIsUpdating(true);
 
+		let filteredTodos: Todo[] = todos;
+
+		if (filter === Filters.completed) {
+			filteredTodos = todos.filter((todo) => todo.completed);
+		}
+
+		if (filter === Filters.active) {
+			filteredTodos = todos.filter((todo) => !todo.completed);
+		}
+
+		if (filteredTodos.length === 0) {
+			return;
+		}
+
+		filteredTodos = filteredTodos.map((todo) => ({
+			...todo,
+			completed: !todo.completed,
+		}));
+
 		todosApi
-			.updateAll(
-				todos
-					.filter((todo) => todo.completed !== completeStatus)
-					.map((todo) => ({ ...todo, completed: completeStatus }))
-			)
+			.updateAll(filteredTodos)
 			.then(loadTodos)
 			.finally(() => {
-				setCompleteStatus(!completeStatus);
 				setIsUpdating(false);
 			});
 	}
