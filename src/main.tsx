686232240970a0b44a8de9b87b987be2c9b635d4
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-05-02 00:23:35 +0300

    build: add sass

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./index.scss";
 const App = React.lazy(() => import("./App.tsx"));
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
